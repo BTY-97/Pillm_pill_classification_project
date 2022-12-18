89f6bbfb4ff51513e68c5a65b25cb080f2e57e96
BTY-97 <imbhang3@naver.com>
2022-12-18 21:31:46 +0900

    predict update

--- fastapi_model_test/predict.py
+++ fastapi_model_test/predict.py
@@ -177,13 +177,11 @@ class Predict():
             for t in texts_list:
                 score_f = SequenceMatcher(None, ''.join(t), front).ratio()
                 if score_f == 1:
-                    score_list.append(10)
-                    break
+                    score_f = 10
                 score_b = SequenceMatcher(None, ''.join(t), back).ratio()
                 if score_b == 1:
-                    score_list.append(10)
-                    break
-                score_list.append(max(score_f, score_b))
+                    score_b = 10
+                score_list.append(max(score_f, score_b)+score)
             scores.append(max(score_list))
         res['score'] = scores
 
